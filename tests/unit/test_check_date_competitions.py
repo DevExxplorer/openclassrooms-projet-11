484d92a75ae120fa3ac6080824aebaee1f7e3b1a
devexplorer <contact@devexplorer.fr>
2025-05-13 09:58:00 -0400

    improvement server.py and add new test

---
+++ tests/unit/test_check_date_competitions.py
@@ -0,0 +1,11 @@
+from datetime import timedelta, datetime
+
+from server import check_date_competitions
+
+
+def test_check_date_competitions():
+    assert check_date_competitions(None) is None
+    assert check_date_competitions("") is None
+
+    past_date = "2020-01-01 10:00:00"
+    assert check_date_competitions(past_date) == 'La compétition est déjà passée'
